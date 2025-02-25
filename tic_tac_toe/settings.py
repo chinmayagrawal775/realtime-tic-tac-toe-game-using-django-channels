119d839e4218a93a27f4eaa88f13719ea2c81756
Chinmay Agrawal <chinmayagrawal775@gmail.com>
2025-02-26 03:06:43 +0530

    Update: Channel layer config.

--- tic_tac_toe/settings.py
+++ tic_tac_toe/settings.py
@@ -139,7 +139,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("localhost", 6379)],
+            "hosts": [("redis://default:HjadMIQRjZfElMxrtZGbWqzYULkdqFCI@redis.railway.internal:6379")],
         },
     },
 }
